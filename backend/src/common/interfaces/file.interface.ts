b6ee187050a2e82d1be129584fa365844db7365d
Raza <mdsaraza@deloitte.com>
2026-01-14 20:04:21 +0530

    Fix prettier and lint issues

--- backend/src/common/interfaces/file.interface.ts
+++ backend/src/common/interfaces/file.interface.ts
@@ -1,56 +1,52 @@
-
-
-enum TranType {
-    CANCEL = '1',
-    APPLICATION = '2'
-}
-
+// enum TranType {
+//   CANCEL = '1',
+//   APPLICATION = '2',
+// }
 
 export interface CraHeader {
-    tranCode: string;      // 6133
-    version: string;        // V00.0
-    processDate: string;    // YYYYMMDD  
-    businessNum: string;    // 15
-    recordCount: number;     // header = 6133V00.020260110885633354RA000100000002
+  tranCode: string // 6133
+  version: string // V00.0
+  processDate: string // YYYYMMDD
+  businessNum: string // 15
+  recordCount: number // header = 6133V00.020260110885633354RA000100000002
 }
 export interface CraDetail {
-    tranCode: string;                 // Record type (detail)
-    referenceNum: string;           // Ministry file reference ID
-    businessNum: string;            // CRA business number
-    tranType: string;            // 1 = Cancellation, 2 = Application
+  tranCode: string // Record type (detail)
+  referenceNum: string // Ministry file reference ID
+  businessNum: string // CRA business number
+  tranType: string // 1 = Cancellation, 2 = Application
 
-    childGivenName: string;         // Child first name
-    childInitial: string;           // Child middle initial
-    childSurName: string;           // Child last name
+  childGivenName: string // Child first name
+  childInitial: string // Child middle initial
+  childSurName: string // Child last name
 
-    childGivenNameAka: string;      // Child alternate first name
-    childSurNameAka: string;        // Child alternate last name
+  childGivenNameAka: string // Child alternate first name
+  childSurNameAka: string // Child alternate last name
 
-    childBirthDate: string;         // Child birth date (YYYYMMDD)
-    childSex: string;               // Child gender code
-    childBirthCity: string;         // Child birth city
-    childBirthProv: string;         // Child birth province code
-    childBirthCountry: string;      // Child birth country code
+  childBirthDate: string // Child birth date (YYYYMMDD)
+  childSex: string // Child gender code
+  childBirthCity: string // Child birth city
+  childBirthProv: string // Child birth province code
+  childBirthCountry: string // Child birth country code
 
-    prevRecipSin: string;   // Social Insurance Number (SIN) of the previous benefit recipient
-    filler1: string;        // Blank filler field used only to keep CRA file format aligned
-    prevRecipGivenName: string;     // Previous recipient first name
-    prevRecipSurName: string;       // Previous recipient last name
+  prevRecipSin: string // Social Insurance Number (SIN) of the previous benefit recipient
+  filler1: string // Blank filler field used only to keep CRA file format aligned
+  prevRecipGivenName: string // Previous recipient first name
+  prevRecipSurName: string // Previous recipient last name
 
-    appStartDate: string;           // Benefit start date
-    newBornCode: string;            // Newborn indicator (Y/N)
+  appStartDate: string // Benefit start date
+  newBornCode: string // Newborn indicator (Y/N)
 
-    cancelEndDate: string;          // Cancellation end date
-    cancelReasonCode: string;       // Cancellation reason code
+  cancelEndDate: string // Cancellation end date
+  cancelReasonCode: string // Cancellation reason code
 
-    ccraDinNum: string;             // CRA document ID number
+  ccraDinNum: string // CRA document ID number
 }
 
-
 export interface CraTrailer {
-    tranCode: string;
-    version: string;
-    processDate: string;
-    businessNum: string;
-    recordCount: number;
+  tranCode: string
+  version: string
+  processDate: string
+  businessNum: string
+  recordCount: number
 }
--- backend/src/cra-data-handling/fileTransfer.service.ts
+++ backend/src/cra-data-handling/fileTransfer.service.ts
@@ -1,41 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import axios from 'axios';
-import * as fs from 'fs';
-import FormData from 'form-data';
-import { COMMON_CONSTANT } from 'src/common/constant/common.constant';
+import { Injectable } from '@nestjs/common'
+import axios from 'axios'
+import * as fs from 'fs'
+import FormData from 'form-data'
+import { COMMON_CONSTANT } from 'src/common/constant/common.constant'
 
-const { SERVICE_NAME, FTP_BASE_URL } = COMMON_CONSTANT;
+const { SERVICE_NAME, FTP_BASE_URL } = COMMON_CONSTANT
 
 @Injectable()
 export class FileTransferClientService {
+  async sendFileToTransferService(
+    filePath: string,
+    fileName: string,
+    craUserId: string = 'testuser',
+  ): Promise<any> {
+    const formData = new FormData()
 
-    async sendFileToTransferService(
-        filePath: string,
-        fileName: string,
-        craUserId: string = 'testuser',
-    ): Promise<any> {
+    formData.append('file', fs.createReadStream(filePath), fileName)
 
-        const formData = new FormData();
+    const response = await axios.post(FTP_BASE_URL, formData, {
+      headers: {
+        ...formData.getHeaders(),
+        uesrid: craUserId,
+        servicename: SERVICE_NAME,
+      },
+      timeout: 30000,
+    })
 
-        formData.append(
-            'file',
-            fs.createReadStream(filePath),
-            fileName
-        );
-
-        const response = await axios.post(
-            FTP_BASE_URL,
-            formData,
-            {
-                headers: {
-                    ...formData.getHeaders(),
-                    uesrid: craUserId,
-                    servicename: SERVICE_NAME
-                },
-                timeout: 30000,
-            }
-        );
-
-        return response.data;
-    }
+    return response.data
+  }
 }

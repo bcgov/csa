659e72b77be771e78c97397f26c893b3f2067dea
Raza <mdsaraza@deloitte.com>
2026-01-16 16:40:44 +0530

    format code in server config

--- backend/src/common/config/server.config.ts
+++ backend/src/common/config/server.config.ts
@@ -16,9 +16,9 @@ for (const key in server_config) {
 }
 
 export const SERVER_CONFIG = {
-  SERVICE_NAME: process.env.SERVICE_NAME || 'csa-backend',
-  FTP_BASE_URL: process.env.FTP_BASE_URL || 'http://localhost:4000/api/transfers',
-  FILE_CREATED_PATH: process.env.FILE_CREATED_PATH || './temp/',
-  FILE_CREATION_ENVIROMENT: process.env.NODE_ENV === 'production' ? 'PCSAIN' : 'ACSAIN',
-  FILE_TYPE_APPLICATION: process.env.NODE_ENV === 'production' ? 'PAPL' : 'AAPL',
+  SERVICE_NAME: SERVICE_NAME || 'csa-backend',
+  FTP_BASE_URL: FTP_BASE_URL || 'http://localhost:4000/api/transfers',
+  FILE_CREATED_PATH: FILE_CREATED_PATH || './temp/',
+  FILE_CREATION_ENVIROMENT: NODE_ENV === 'production' ? 'PCSAIN' : 'ACSAIN',
+  FILE_TYPE_APPLICATION: NODE_ENV === 'production' ? 'PAPL' : 'AAPL',
 }

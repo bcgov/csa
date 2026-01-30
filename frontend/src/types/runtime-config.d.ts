1c1f63474c77485057a729c15ab4c2cf32518a32
Lakkaraju <nalakkaraju@deloitte.ca>
2026-01-30 12:16:22 -0500

    end to end integration with contacts endpoint and added filters and search facility for all tables

--- frontend/src/types/runtime-config.d.ts
+++ frontend/src/types/runtime-config.d.ts
@@ -4,6 +4,7 @@ export interface RuntimeConfig {
   VITE_KEYCLOAK_URL: string
   VITE_KEYCLOAK_REALM: string
   VITE_KEYCLOAK_CLIENT_ID: string
+  VITE_API_BASE_URL?: string
 }
 
 declare global {
@@ -12,4 +13,5 @@ declare global {
   }
 }
 
-export {}
+export { }
+

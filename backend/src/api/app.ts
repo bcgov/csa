1af09e7ad61fe04cb057fa59d8410a752a3b8b7b
saifrazabc <Saif.Raza@gov.bc.ca>
2026-02-03 20:45:50 +0530

    feat: flat file creation for application and cancellation (#33)
    cra file creation workflow with mock data

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -3,12 +3,9 @@ import type { NestExpressApplication } from '@nestjs/platform-express'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import helmet from 'helmet'
 import { metricsMiddleware } from 'src/common/middleware/prom'
-import { customLogger } from '../common/config/logger.config'
+import { customLogger } from '../common/logger/logger.config'
 import { ApiModule } from './api.module'
 
-/**
- *
- */
 export async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(ApiModule, {
     logger: customLogger,
@@ -21,9 +18,14 @@ export async function bootstrap() {
   app.setGlobalPrefix('api', {
     exclude: ['health', 'health/live', 'health/ready'],
   })
-  const config = new DocumentBuilder().setTitle('CSA API').setDescription('CSA BACKEND API').build()
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('CSA API')
+      .setDescription('CSA BACKEND API')
+      .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('/api/docs', app, document)
+  }
 
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('/api/docs', app, document)
   return app
 }
---
+++ backend/src/cra/outbound-file/file-mock-data.ts
@@ -0,0 +1,106 @@
+import { CRA_DATA_HANDLING_CONSTANT } from '../cra.constant'
+
+const { HEADER_TRAN_CODE, DETAIL_TRAN_CODE, TRAILER_TRAN_CODE } =
+  CRA_DATA_HANDLING_CONSTANT.REQUEST_FILE
+
+const currentDate = (): string => {
+  const now = new Date()
+  const year = now.getFullYear()
+  const month = String(now.getMonth() + 1).padStart(2, '0')
+  const day = String(now.getDate()).padStart(2, '0')
+  return `${year}${month}${day}`
+}
+
+const details = [
+  {
+    tranCode: DETAIL_TRAN_CODE,
+    referenceNum: 'REF00000000000001',
+    businessNum: '885633354RA0001',
+    tranType: 2, // 2 = Application
+
+    childGivenName: 'JOHN',
+    childInitial: 'D',
+    childSurName: 'DOE',
+
+    childGivenNameAka: '',
+    childSurNameAka: '',
+
+    childBirthDate: '20200115',
+    childSex: 'M',
+    childBirthCity: 'TORONTO',
+    childBirthProv: 'ON',
+    childBirthCountry: 'CA',
+
+    prevRecipSin: '123456789', // Previous Recipient Social Insurance Number (SIN)
+    filler1: '      ', // Always blank for CRA future use
+
+    prevRecipGivenName: 'MARY', // Previous Recipient Given Name
+    prevRecipSurName: 'DOE', // Previous Recipient Surname
+
+    appStartDate: '20260101',
+    newBornCode: 'Y', // Y = Yes N = No
+    filler2: '          ',
+
+    cancelEndDate: '',
+    cancelReasonCode: '',
+    ccraDinNum: '987654321', // CCRA Document Identification Number
+    filler3: '               ',
+  },
+  {
+    tranCode: DETAIL_TRAN_CODE,
+    referenceNum: 'REF00000000000002',
+    businessNum: '885633354RA0001',
+    tranType: 1, // 2 = Application
+
+    childGivenName: 'JOHN',
+    childInitial: 'D',
+    childSurName: 'DOE',
+
+    childGivenNameAka: '',
+    childSurNameAka: '',
+
+    childBirthDate: '20200115',
+    childSex: 'M',
+    childBirthCity: 'TORONTO',
+    childBirthProv: 'ON',
+    childBirthCountry: 'CA',
+
+    prevRecipSin: '123456789', // Previous Recipient Social Insurance Number (SIN)
+    filler1: '', // Always blank for CRA future use
+
+    prevRecipGivenName: 'MARY', // Previous Recipient Given Name
+    prevRecipSurName: 'DOE', // Previous Recipient Surname
+
+    appStartDate: '20260101',
+    newBornCode: 'Y', // Y = Yes N = No
+    filler2: '',
+
+    cancelEndDate: '',
+    cancelReasonCode: '',
+    ccraDinNum: '987654321', // CCRA Document Identification Number
+    filler3: '',
+  },
+]
+
+export const FILE_MOCK_DATA = {
+  // DATA FOR TESTING PURPOSES
+  header: {
+    tranCode: HEADER_TRAN_CODE,
+    versionNum: 'V00.0',
+    processDate: currentDate(), // YYYYMMDD
+    businessNum: '885633354RA0001', // 15 chars
+    recordCount: 0, // must match detail records
+    filler: '                         ',
+  },
+  // 6133V00.020260110885633354RA000100000002
+  details: details,
+
+  trailer: {
+    tranCode: TRAILER_TRAN_CODE,
+    versionNum: 'V00.0',
+    processDate: currentDate(),
+    businessNum: '885633354RA0001',
+    recordCount: details.length,
+    filler: '',
+  },
+}

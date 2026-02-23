1acd181c6e86988dee7b1f82bcb2e2eee29ce005
plakkara <preethi.lakkaraju@gov.bc.ca>
2026-02-22 22:09:08 -0500

    prevented filter reset and modified displayed username

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -14,6 +14,7 @@ interface AuthContextType {
     name?: string
     email?: string
     username?: string
+    idirUsername?: string
     roles?: string[]
   } | null
   login: () => void
@@ -100,6 +101,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
                       name: keycloakInstance.tokenParsed.name,
                       email: keycloakInstance.tokenParsed.email,
                       username: keycloakInstance.tokenParsed.preferred_username,
+                      idirUsername: keycloakInstance.tokenParsed.idir_username?.toUpperCase(),
                       roles: keycloakInstance.tokenParsed.realm_access?.roles || [],
                     })
                     setIsLoading(false)

8f26de853aca78b4703391ab19a86643e06483c2
Madjo Diapena <Madjo.Diapena@gov.bc.ca>
2026-03-15 13:19:22 -0400

    fix tests failing bercause of required db password

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -3,21 +3,16 @@ const buildDataSourceUrl = (): string => {
     return process.env.DATABASE_URL
   }
 
-  const host = process.env.POSTGRES_HOST || 'localhost'
-  const user = process.env.POSTGRES_USER || 'postgres'
+  const host = process.env.POSTGRES_HOST
+  const user = process.env.POSTGRES_USER
   const password = process.env.POSTGRES_PASSWORD
-  if (!password) throw new Error('POSTGRES_PASSWORD environment variable is required')
   const port = process.env.POSTGRES_PORT || 5432
-  const database = process.env.POSTGRES_DATABASE || 'postgres'
+  const database = process.env.POSTGRES_DATABASE
 
   return `postgresql://${user}:${encodeURIComponent(password)}@${host}:${port}/${database}`
 }
 
 export const databaseConfig = {
-  get url() {
-    return buildDataSourceUrl()
-  },
-  get schema() {
-    return process.env.POSTGRES_SCHEMA || 'csa'
-  },
+  url: buildDataSourceUrl(),
+  schema: process.env.POSTGRES_SCHEMA || 'csa',
 }

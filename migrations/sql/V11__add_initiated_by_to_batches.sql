f486c5fcf786cdb33ef09550ce362b1cf61e26a1
Madjo Diapena <Madjo.Diapena@gov.bc.ca>
2026-04-30 00:23:51 -0400

    initiated_by migration

--- migrations/sql/V11__add_initiated_by_to_batches.sql
+++ migrations/sql/V11__add_initiated_by_to_batches.sql
@@ -1,4 +1,1 @@
-
-ALTER TABLE csa.batches ADD COLUMN initiated_by VARCHAR(20) NOT NULL DEFAULT 'Ministry';
-COMMENT ON COLUMN csa.batches.initiated_by IS 'Who created 
-the batch: Ministry (user-created), CRA (auto-created from weekly CRA file)';
+ALTER TABLE csa.batches ADD COLUMN initiated_by TEXT NOT NULL;

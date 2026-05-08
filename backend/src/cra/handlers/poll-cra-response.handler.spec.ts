799a1fb3cbc2b64f3faf85f8d8ed44f4dd8a7402
Raza <mdsaraza@deloitte.com>
2026-05-08 19:02:09 +0530

    fix prieter format

--- backend/src/cra/handlers/poll-cra-response.handler.spec.ts
+++ backend/src/cra/handlers/poll-cra-response.handler.spec.ts
@@ -898,7 +898,7 @@ describe('PollCraResponseHandler', () => {
       careStartDate: '20250101',
       careEndDate: '        ',
       careEndReasonCode: '  ',
-      status: WKL_STATUS.COMPLETED ,
+      status: WKL_STATUS.COMPLETED,
       completionDate: '20250420',
       ...overrides,
     })

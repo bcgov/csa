a17ce1e309aed729ebd8853e17e60bf8834cb6bd
plakkara-bc <Preethi.Lakkaraju@gov.bc.ca>
2026-05-19 12:20:09 -0400

    fix: fixed migrations issue (#273)
    * fixed migrations issue
    
    * fixed migrations issue
    
    * added migrations to validate paths

--- migrations/sql/V12__add_data_changed_at_to_staging.sql
+++ migrations/sql/V12__add_data_changed_at_to_staging.sql
@@ -3,9 +3,9 @@
 -- (TIMESTAMPTZ) only updates when meaningful field values actually change.
 -- This prevents false "data changed" signals from our own CSA status sync-back.
 
-ALTER TABLE stg_icm_cases ADD COLUMN data_changed_at TIMESTAMPTZ;
-ALTER TABLE stg_icm_placements ADD COLUMN data_changed_at TIMESTAMPTZ;
-ALTER TABLE stg_icm_legal_authority ADD COLUMN data_changed_at TIMESTAMPTZ;
-ALTER TABLE stg_icm_legal_authority_admin ADD COLUMN data_changed_at TIMESTAMPTZ;
-ALTER TABLE stg_icm_agreement ADD COLUMN data_changed_at TIMESTAMPTZ;
-ALTER TABLE stg_icm_orders ADD COLUMN data_changed_at TIMESTAMPTZ;
+ALTER TABLE csa.stg_icm_cases ADD COLUMN data_changed_at TIMESTAMPTZ;
+ALTER TABLE csa.stg_icm_placements ADD COLUMN data_changed_at TIMESTAMPTZ;
+ALTER TABLE csa.stg_icm_legal_authority ADD COLUMN data_changed_at TIMESTAMPTZ;
+ALTER TABLE csa.stg_icm_legal_authority_admin ADD COLUMN data_changed_at TIMESTAMPTZ;
+ALTER TABLE csa.stg_icm_agreement ADD COLUMN data_changed_at TIMESTAMPTZ;
+ALTER TABLE csa.stg_icm_orders ADD COLUMN data_changed_at TIMESTAMPTZ;

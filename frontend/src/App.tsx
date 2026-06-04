748201e78e1168e77596ae6db15cacde6333ce21
plakkara <preethi.lakkaraju@gov.bc.ca>
2026-06-04 17:41:16 -0400

    modified reason column to expand sideways and added tooltip display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3909,7 +3909,13 @@ function App() {
                           <TableCell>{row.caseStatus}</TableCell>
                           <TableCell>{row.legacyFile}</TableCell>
                           <TableCell>{row.cgwrks3 || ''}</TableCell>
-                          <TableCell>
+                          <TableCell
+                            sx={
+                              row.holdReason && row.holdReason.length > HOLD_REASON_PREVIEW_LENGTH
+                                ? { minWidth: 460, maxWidth: 520 }
+                                : undefined
+                            }
+                          >
                             <Box sx={{ display: 'flex', alignItems: 'flex-start', gap: 0.5 }}>
                               {row.holdReason ? (
                                 row.holdReason.length > HOLD_REASON_PREVIEW_LENGTH ? (
@@ -3917,7 +3923,7 @@ function App() {
                                     <Typography
                                       component="span"
                                       sx={{
-                                        maxWidth: 300,
+                                        maxWidth: 500,
                                         whiteSpace: 'pre-wrap',
                                         wordBreak: 'break-word',
                                         fontSize: 'inherit',
